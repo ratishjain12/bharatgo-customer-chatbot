84ad40958944130573e814c8119195f342b27acc
ratish jain <ratishjain6@gmail.com>
2025-09-23 17:22:12 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    rollupOptions:{
+      input:{
+        index: resolve(__dirname, "index.html"),
+        embed: resolve(__dirname, "embed.html"),
+      }
+    }
+  }
 })
